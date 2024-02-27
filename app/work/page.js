aba08a5b8797ba6ff6e9e223f9bf6a746caa6038
mdLafrance <maxlafrance97@gmail.com>
2024-02-27 16:58:36 -0500

    Working on work history page

--- app/work/page.js
+++ app/work/page.js
@@ -1,4 +1,53 @@
-import { PythonChicklet } from "@/lib/skills";
+import pythonIcon from "../../public/python.svg";
+import bashIcon from "../../public/bash.svg";
+import githubActionsIcon from "../../public/github_actions.svg";
+import nukeIcon from "../../public/nuke.svg";
+import gitlabIcon from "../../public/gitlab.svg";
+
+import Image from 'next/image';
+
+function WorkExperienceChicklet({ icon, name, link }) {
+    return (
+        <a href={link} target="_blank" rell="noopener noreferrer" className="
+            h-8 px-3 flex items-center space-x-1 
+            ring-1 ring-white/25 rounded-xl backdrop-blur-3xl bg-white/10 hover:bg-white/25 transition-all
+        ">
+            <Image src={icon} alt={name} width={23} height={23} />
+            <span className="font-Kanit font-regular text-white/80">{name}</span>
+        </a>
+    )
+}
+
+export function PythonChicklet() {
+    return (
+        <WorkExperienceChicklet icon={pythonIcon} name={"python"} link={"https://www.python.org/"} />
+    )
+}
+
+export function BashChicklet() {
+    return (
+        <WorkExperienceChicklet icon={bashIcon} name={"bash"} link={"https://opensource.com/resources/what-bash"} />
+    )
+}
+
+export function ActionsChicklet() {
+    return (
+        <WorkExperienceChicklet icon={githubActionsIcon} name={"CI/CD"} link={"https://about.gitlab.com/solutions/continuous-integration/"} />
+    )
+}
+
+export function NukeChicklet() {
+    return (
+        <WorkExperienceChicklet icon={nukeIcon} name={"Nuke"} link={"https://www.foundry.com/products/nuke-family/nuke"} />
+    )
+}
+
+export function GitlabChicklet() {
+    return (
+        <WorkExperienceChicklet icon={gitlabIcon} name={"Gitlab"} link={"https://about.gitlab.com/"} />
+    )
+}
+
 
 const work_experiences = [
     {
@@ -14,16 +63,22 @@ Often acted as primary technical liaison within the company when onboarding new 
 Advocated for and implemented better observability and maintainability patterns within the codebase, including implementing and maintaining CI / CD pipelines for linting, testing, and automatic documentation generation.\
 ",
         skills: [
+            PythonChicklet,
+            BashChicklet,
+            ActionsChicklet,
+            NukeChicklet,
+            GitlabChicklet
         ]
     }
 ]
 
-function WorkHistoryCard({ title, company, summary, date }) {
+function WorkHistoryCard({ title, company, summary, date, skills }) {
     return (
         <div className="
+            group
             rounded-xl py-4 px-6
-            hover:backdrop-blur-3xl hover:bg-white/10 transition-all duration-100
-            hover:shadow-black/20 hover:shadow-sm
+            hover:backdrop-blur-3xl hover:bg-white/10 transition-all
+            hover:shadow-black/5 hover:shadow-md
             w-[40rem]
         ">
             {/** Left/right div **/}
@@ -33,10 +88,22 @@ function WorkHistoryCard({ title, company, summary, date }) {
                     <p className="text-white/80 font-Kanit font-light text-lg">{date}</p>
                 </div>
                 {/** Experience **/}
-                <div className="">
+                <div>
                     <p className="font-Kanit font-bold text-white text-2xl">{title}</p>
                     <p className="font-Kanit font-semibold text-white/80 text-xl">{company}</p>
-                    <p className="text-white/70 font-Lora font-regular text-lg">{summary}</p>
+                    <p className="text-white/70 group-hover:text-white font-Lora font-regular text-lg transition-all">{summary}</p>
+                    {/** Work experience chicklets **/}
+                    <div className="flex flex-wrap gap-2 mt-2">
+                        {
+                            skills.map((S, idx) => {
+                                return (
+                                    <div className="grow-0">
+                                        <S key={idx} />
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </div>
         </div>
@@ -50,16 +117,15 @@ export default function Work() {
             <div>
             </div>
 
-            {/** Work history cards 
+            {/** Work history cards **/}
             <div className="flex flex-col">
                 {
                     work_experiences.map((e) => {
-                        return <WorkHistoryCard title={e.title} company={e.company} date={e.date} summary={e.summary} />
+                        return <WorkHistoryCard title={e.title} company={e.company} date={e.date} summary={e.summary} skills={e.skills} />
                     })
                 }
             </div>
-            **/}
-            <PythonChicklet />
+
         </main>
     )
 }
