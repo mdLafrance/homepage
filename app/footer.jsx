6fedef2cbf626ef2e0b87ebbbc188002d7e49e2c
mdLafrance <maxlafrance97@gmail.com>
2024-02-27 09:43:43 -0500

    Fixes footer rule

--- app/footer.jsx
+++ app/footer.jsx
@@ -6,38 +6,34 @@ import emailIcon from '../public/email.svg';
 
 function FooterIconLink({ icon, link }) {
     return (
-        <a href={link} target="_blank" rel="noopener noreferrer" className='
-
-        '>
-            <Image src={icon} width={30} />
+        <a href={link} target="_blank" rel="noopener noreferrer">
+            <Image src={icon} width={25} />
         </a >
     )
 }
 
 function FooterIconSpacer() {
     return (
-        <div className='grow-0'>
-            <span className='text-3xl text-opacity-35 text-black'>/</span>
-        </div>
+        <span className='text-3xl text-opacity-35 text-black'>/</span>
     )
 }
 
 function Rule() {
     return (
-        <hr className='grow-1 border-t-[2px] h-1 w-48 border-black/20 ' />
+        <hr className='flex-1 border-t-[2px] h-1 border-black/20' />
     )
 }
 
 export default function Footer() {
     return (
-        <div className="min-h-32 mt-12 ">
-            <div className="flex justify-center space-x-6 items-center">
+        <div className="min-h-32 mt-12 w-full">
+            <div className='flex flex-row gap-4 items-center '>
                 <Rule />
                 <FooterIconLink icon={githubIcon} link={"https://github.com/mdLafrance"} />
                 <FooterIconSpacer />
                 <FooterIconLink icon={linkedinIcon} link={"https://www.linkedin.com/in/max-lafrance/"} />
                 <FooterIconSpacer />
-                <FooterIconLink icon={emailIcon} link={"mailto:maxlafrance97@gmail.com"}/>
+                <FooterIconLink icon={emailIcon} link={"mailto:maxlafrance97@gmail.com"} />
                 <Rule />
             </div>
         </div>
