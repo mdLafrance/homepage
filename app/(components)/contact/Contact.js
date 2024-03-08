82b9f7f018949780429db2f84c1fab29aa692bf2
mdLafrance <maxlafrance97@gmail.com>
2024-03-08 14:16:31 -0500

    Formatting for contact section

--- app/(components)/contact/Contact.js
+++ app/(components)/contact/Contact.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useWaveContext } from "../../(context)/WaveContext"
 import LinkSvg from "../controls/Link"
-import LinkSVG from "../controls/Link"
 
 function ContactItem({ name, footerText }) {
     const [waveSettings, _] = useWaveContext();
@@ -17,14 +16,18 @@ function ContactItem({ name, footerText }) {
     }, [waveSettings])
 
     return (
-        <button className="flex gap-1 items-end">
+        <button className="
+            flex gap-1 items-end pt-1 px-2
+            hover:ring-1 hover:ring-white/30 hover:bg-white/10
+            transition-all duration-[80ms]
+        ">
             <span className="text-white">{name}</span>
             <div className="grow grow-1 ">
                 <div className="border-b border-dotted -translate-y-2 border-white/40">
                 </div>
             </div>
             <span style={{ color: formattedColor }} className="">{footerText}</span>
-            <LinkSvg color={formattedColor} weight={6} className="w-3 h-3 -translate-y-[0.5px] self-center" />
+            <LinkSvg color={formattedColor} weight={6} className="w-3 h-3 -translate-y-[2px] self-center" />
         </button>
     )
 }
@@ -36,9 +39,9 @@ export function ContactSection() {
                 <p className="text-white text-3xl translate-y-1 translate-x-1">Contact</p>
             </div>
             <div className="flex flex-col justify-start gap-1 m-1">
-                <ContactItem name={'Email'} footerText={"maxlafrance97@gmail.com"} />
-                <ContactItem name={'Linkedin'} footerText={"linkedin.com/in/max-lafrance"} />
                 <ContactItem name={'Github'} footerText={"github.com/mdLafrance"} />
+                <ContactItem name={'Linkedin'} footerText={"linkedin.com/in/max-lafrance"} />
+                <ContactItem name={'Email'} footerText={"maxlafrance97@gmail.com"} />
             </div>
         </div>
     )
--- app/(components)/controls/Link.jsx
+++ app/(components)/controls/Link.jsx
@@ -3,7 +3,7 @@ export default function LinkSvg({color, weight, className}) {
     return (
         <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" className={className}>
             <path d="M0 0 L100 0 L100 100" fill="none" stroke={color} stroke-width={2 * weight} />
-            <path d="M0 100 L100 0" fill="none" stroke={color} stroke-width={weight} />
+            <path d="M0 100 L90 10" fill="none" stroke={color} stroke-width={weight} />
         </svg>
     )
 }
