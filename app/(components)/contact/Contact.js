1bf103435d8e85ea95697cacb3524973ae638266
mdLafrance <maxlafrance97@gmail.com>
2024-03-08 14:24:33 -0500

    Contact button links working

--- app/(components)/contact/Contact.js
+++ app/(components)/contact/Contact.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useWaveContext } from "../../(context)/WaveContext"
 import LinkSvg from "../controls/Link"
 
-function ContactItem({ name, footerText }) {
+function ContactItem({ name, footerText, link }) {
     const [waveSettings, _] = useWaveContext();
     const [formattedColor, setFormattedColor] = useState("rgb(255, 255, 255)");
 
@@ -16,11 +16,18 @@ function ContactItem({ name, footerText }) {
     }, [waveSettings])
 
     return (
-        <button className="
-            flex gap-1 items-end pt-1 px-2
-            hover:ring-1 hover:ring-white/30 hover:bg-white/10
-            transition-all duration-[80ms]
-        ">
+        <a 
+            className="
+                flex gap-1 items-end pt-1 px-2
+                hover:ring-1 hover:ring-white/30 hover:bg-white/10
+                active:bg-white/20
+                transition-all duration-[80ms]
+                "
+            
+            href={link}
+            rel="noopener noreferrer"
+            target="_blank"
+        >
             <span className="text-white">{name}</span>
             <div className="grow grow-1 ">
                 <div className="border-b border-dotted -translate-y-2 border-white/40">
@@ -28,7 +35,7 @@ function ContactItem({ name, footerText }) {
             </div>
             <span style={{ color: formattedColor }} className="">{footerText}</span>
             <LinkSvg color={formattedColor} weight={6} className="w-3 h-3 -translate-y-[2px] self-center" />
-        </button>
+        </a>
     )
 }
 
@@ -38,10 +45,10 @@ export function ContactSection() {
             <div className="border-b border-solid border-white/20">
                 <p className="text-white text-3xl translate-y-1 translate-x-1">Contact</p>
             </div>
-            <div className="flex flex-col justify-start gap-1 m-1">
-                <ContactItem name={'Github'} footerText={"github.com/mdLafrance"} />
-                <ContactItem name={'Linkedin'} footerText={"linkedin.com/in/max-lafrance"} />
-                <ContactItem name={'Email'} footerText={"maxlafrance97@gmail.com"} />
+            <div className="flex flex-col justify-start gap-1 p-1 ">
+                <ContactItem name={'github'} footerText={"github.com/mdLafrance"} link={"https://github.com/mdLafrance"} />
+                <ContactItem name={'linkedin'} footerText={"linkedin.com/in/max-lafrance"} link={"https://linkedin.com/in/max-lafrance"} />
+                <ContactItem name={'email'} footerText={"maxlafrance97@gmail.com"} link={"mailto: maxlafrance97@gmail.com"} />
             </div>
         </div>
     )
