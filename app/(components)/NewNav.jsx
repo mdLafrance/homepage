eeaee3982320c7ee4636e7659bb2092ffdf5c31c
mdLafrance <maxlafrance97@gmail.com>
2024-04-03 15:41:04 -0400

    Copies some theme switching logic from controlbar to newnav

--- app/(components)/NewNav.jsx
+++ app/(components)/NewNav.jsx
@@ -1,24 +1,37 @@
 "use client"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useThemeContext } from "../(context)/ThemeContext";
 import ControlBar from "./controls/ControlBar"
 import Link from "next/link";
 
-function NavItem({ name, linkTarget, isSelected, onClick }) {
+function NavItem({ name, linkTarget, isSelected, onClick, className }) {
     return (
-        <Link href={linkTarget} onClick={onClick} className="
+        <Link href={linkTarget} onClick={onClick} className={`
             font-NeueMachina text-2xl 
             opacity-85 hover:opacity-100 
             transition-all
-        ">
+            ${className}
+        `}>
             {name}
         </Link>
     )
 }
 
 export default function NewNav() {
+    const darkInteractionTheme =  "text-light hover:bg-light/10 active:bg-light/20"
+    const lightInteractionTheme = "text-space_cadet hover:bg-space_cadet/10 active:bg-black/20"
+
     const [theme, _] = useThemeContext();
+    const [interactionTheme, setInteractionTheme] = useState("")
+
+    useEffect(() => {
+        if (theme == "light") {
+            setInteractionTheme(lightInteractionTheme)
+        } else {
+            setInteractionTheme(darkInteractionTheme)
+        }
+    }, [theme])
 
     const pageNames = [
         ["ABOUT", "/"],
@@ -34,7 +47,6 @@ export default function NewNav() {
             z-100
             flex justify-start items-center gap-4
             h-[3rem]
-
         `}>
         {
             pageNames.map(([name, route], idx) => (
@@ -43,6 +55,7 @@ export default function NewNav() {
                     isSelected={currentPageName === name}
                     linkTarget={route}
                     onClick={() => setCurrentPageName(name)}
+                    className={interactionTheme}
                 />
             ))
         }
