310941f5b9d2cc5b7b7972cdfab394d026968f23
mdLafrance <maxlafrance97@gmail.com>
2024-04-10 01:45:11 -0400

    Basic stuff for about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,5 +1,36 @@
+"use client"
+
+import { motion } from "framer-motion"
+
+const aboutCardTransitions = {
+    hidden: {
+        opacity: 0
+    },
+    show: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.10
+        }
+    }
+}
+
 export default function About() {
     return (
-        <div></div>
+        <motion.div
+            variants={aboutCardTransitions}
+            initial="hidden"
+            animate="show"
+            className="w-full flex flex-col justify-start text-7xl text-light p-8 gap-4"
+        >
+            <motion.h1 variants={aboutCardTransitions}>
+                .ME
+            </motion.h1>
+            <motion.h1 variants={aboutCardTransitions}>
+                .PROJECTS
+            </motion.h1>
+            <motion.h1 variants={aboutCardTransitions}>
+                .ART
+            </motion.h1>
+        </motion.div>
     )
 }
