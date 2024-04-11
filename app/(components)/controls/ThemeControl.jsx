aea2671ba2bed6e7daeccc068921e6947e7dd154
mdLafrance <maxlafrance97@gmail.com>
2024-04-10 20:22:22 -0400

    Switch to sun svg in dark mode for theme control

--- app/(components)/controls/ThemeControl.jsx
+++ app/(components)/controls/ThemeControl.jsx
@@ -7,7 +7,7 @@ const moon = (
 )
 
 const sun = (
-    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <circle cx="12" cy="12" r="5" stroke="#1C274C" stroke-width="1.5"></circle> <path d="M12 2V4" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M12 20V22" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M4 12L2 12" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M22 12L20 12" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M19.7778 4.22266L17.5558 6.25424" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M4.22217 4.22266L6.44418 6.25424" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M6.44434 17.5557L4.22211 19.7779" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M19.7778 19.7773L17.5558 17.5551" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <circle cx="12" cy="12" r="5" stroke="#FFFFFF" stroke-width="1.5"></circle> <path d="M12 2V4" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round"></path> <path d="M12 20V22" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round"></path> <path d="M4 12L2 12" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round"></path> <path d="M22 12L20 12" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round"></path> <path d="M19.7778 4.22266L17.5558 6.25424" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round"></path> <path d="M4.22217 4.22266L6.44418 6.25424" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round"></path> <path d="M6.44434 17.5557L4.22211 19.7779" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round"></path> <path d="M19.7778 19.7773L17.5558 17.5551" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
 )
 
 export default function ThemeSwitcher() {
@@ -33,7 +33,7 @@ export default function ThemeSwitcher() {
                 strokeLinejoin="round" 
                 className="scale-90 scale-x-[-1] -translate-y-0.2 -translate-x-0.2"
             >
-                {moon}
+                {theme == "dark" ? sun : moon}
             </svg>
 
         </button>
