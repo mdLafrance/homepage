141fa3add908d4c2050f43bd297e0c1ec6602512
mdLafrance <maxlafrance97@gmail.com>
2024-04-12 16:30:20 -0400

    Remove translate on navbar items

--- app/(components)/NewNav.jsx
+++ app/(components)/NewNav.jsx
@@ -16,7 +16,6 @@ function NavItem({ name, linkTarget, isSelected, onClick, className }) {
             p-1 rounded-sm
             w-[95px]
             text-center
-            translate-y-1
             ${className}
         `}>
             <span className={` ${isSelected ? "underline decoration-1 underline-offset-[3px] decoration-dotted" : null}`}>{name}</span>
