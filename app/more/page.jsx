3751e90862f42c9faa60b9d17c0308f05dfc5ef9
mdLafrance <maxlafrance97@gmail.com>
2024-04-19 00:15:39 -0400

    Reduce top padding on more sections

--- app/more/page.jsx
+++ app/more/page.jsx
@@ -107,7 +107,7 @@ function MoreSection({ title, children }) {
                         -translate-y-1 lg:-translate-y-2
                     "/>
                 </div>
-                <div className="sm:text-2xl font-light pt-4 sm:pt-4 sm:ml-1">
+                <div className="sm:text-2xl font-light pt-2 sm:pt-4 sm:ml-1">
                     {children}
                 </div>
             </div>
