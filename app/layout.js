7f7f144bc093e3f08140324786488f31d1fa5809
mdLafrance <maxlafrance97@gmail.com>
2024-05-17 15:26:56 -0400

    Fix typo for speed insights

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { WavyBackground } from "./(components)/BackgroundWaves"
 
